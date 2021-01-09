72c29be5134202fcd643232e53271c199a1f42a8
duarch <andre.bh@hotmail.com>
2021-01-09 14:34:46 -0300

    Some improvements

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-window.
-onload
-    = function() {
-        Particles.
-        init
-            ({
-                // normal options
-                selector: '.background',
-                maxParticles: 50,
-                color: ['#DA0463', '#404B69', '#DBEDF3'],
-                connectParticles: true,
-
-                // options for breakpoints
-                responsive: [{
-                    breakpoint: 768,
-                    options: {
-                        maxParticles: 50,
-                        color: '#ffffff',
-                        connectParticles: true
-                    }
-                }, {
-                    breakpoint: 425,
-                    options: {
-                        maxParticles: 20,
-                        color: '#ffffff',
-                        connectParticles: false
-                    }
-                }, {
-                    breakpoint: 320,
-                    options: {
-                        maxParticles: 0
-                            // disables particles.js
-                    }
-                }]
-            });
-    };
+var d = new Date();
+var n = d.getFullYear();
+document.getElementById("year").innerHTML = n;
